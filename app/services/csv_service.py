d0f9b21b1adc6266623ea39b25dcc71a440db4a0
hyarlei <hyarleysf@gmail.com>
2024-11-20 19:14:45 -0300

    feat: Criada funcionalidade GET para obter um item específico do menu pelo ID

--- app/services/csv_service.py
+++ app/services/csv_service.py
@@ -98,17 +98,13 @@ def buscar_item_por_id(item_id: int):
         if not os.path.exists(MENU_FILE) or os.stat(MENU_FILE).st_size == 0:
             raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Menu vazio")
 
-        # Lê o CSV
         df = pd.read_csv(MENU_FILE)
 
-        # Busca o item pelo ID
         item = df.loc[df["id"] == item_id]
 
-        # Verifica se o item foi encontrado
         if item.empty:
             raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Item não encontrado")
 
-        # Retorna o item encontrado
         return item.to_dict(orient="records")[0]
 
     except HTTPException as e:
