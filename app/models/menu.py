92d24b53ee80b78a949a705ae3c90fcf51acf613
Adailton Bezerra <adailtonbsf@gmail.com>
2024-11-24 09:38:14 -0300

    Refactor: MenuItem tipo

--- app/models/menu.py
+++ app/models/menu.py
@@ -1,10 +1,10 @@
 from pydantic import BaseModel
-
+from typing import Literal
 
 class MenuItem(BaseModel):
     id: int | None = None
     nome: str
     descricao: str
     preco: float
-    tipo: str
+    tipo: Literal["entrada", "principal", "sobremesa", "bebida", "acompanhamento", "outro"]
     disponivel: bool
