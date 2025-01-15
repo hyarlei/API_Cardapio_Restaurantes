cd68dda7f809492d89761afc4b61b56ff62c7d19
hyarlei <hyarleysf@gmail.com>
2025-01-14 23:20:55 -0300

    Fix:Resolvendo bugs

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.routes.menu_routes import router as menu_router
-from app.services.csv_service import init_db
+from app.routes.cliente_routes import router as cliente_router
+from app.routes.pedido_routes import router as pedido_router
+from app.services.menu_sevice import init_db
 
 app = FastAPI()
 
@@ -8,3 +10,5 @@ init_db()
 
 # Incluindo o roteador
 app.include_router(menu_router, prefix="/menu", tags=["Menu"])
+app.include_router(cliente_router, prefix="/cliente", tags=["Cliente"])
+app.include_router(pedido_router, prefix="/pedido", tags=["Pedido"])
